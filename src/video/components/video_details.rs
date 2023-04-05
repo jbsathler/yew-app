a1cf7bbc431843f48641f6c63c636cb87336486f
Jaderson Barreto Sathler <jaderson.sathler@sysmap.com.br>
2023-04-05 17:43:48 -0300

    Embed Youtube videos.

--- src/video/components/video_details.rs
+++ src/video/components/video_details.rs
@@ -7,12 +7,19 @@ pub struct VideoDetailsProps {
   pub video: Video,
 }
 
+fn get_embed_link(url: String) -> String {
+  return url.clone().replace("https://youtu.be/", "https://www.youtube-nocookie.com/embed/");
+}
+
 #[function_component(VideoDetails)]
 pub fn video_details(VideoDetailsProps { video }: &VideoDetailsProps) -> Html {
   html! {
     <div>
       <h3 class={classes!("text-2xl", "font-semibold", "mb-2")}>{ video.title.clone() }</h3>
-      <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+      <iframe width="640" height="360" frameborder="0" allowfullscreen={true} title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        src={ get_embed_link(video.url.clone()) }
+      ></iframe>
     </div>
   }
 }
